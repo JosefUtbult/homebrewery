eebc65785a41cd2e84a7ad23421122498b2ad2a5
Josef Utbult <josef@utbult.design>
2022-09-26 18:37:35 +0200

    Add functionality to buildHomebrew

--- server/homebrew.api.js
+++ server/homebrew.api.js
@@ -252,12 +252,12 @@ const getThemes = (req, res)=>{
 		});
 	});
 
-	// Header stuff for allowing the css to access fonts
+	// Header stuff for allowing sites to make cross origin requests for the json object
 	res.setHeader('Access-Control-Allow-Origin',  '*');
 	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept');
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
 	res.type('json');
-    res.send(themesData);
+	res.send(themesData);
 };
 
 // Checks for a specific theme from the path in the url and sends its style.css
@@ -279,7 +279,7 @@ const getThemeStyle = (req, res)=>{
 			return "url('/api/themes/fonts/" + encodeURIComponent($1) + "/" + encodeURIComponent($2) + "')"
 		});
 
-		// Header stuff for allowing the css to access fonts
+		// Header stuff for allowing sites to make cross origin requests for the stylesheets
 		res.setHeader('Access-Control-Allow-Origin',  '*');
 		res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept');
 		res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
@@ -288,6 +288,7 @@ const getThemeStyle = (req, res)=>{
 	});
 };
 
+// Checks for font files required by the stylesheets and sends it
 const getThemeFonts = (req, res)=>{
 	const path = req.params.path + '/' + req.params.file	
 	options = {
@@ -297,7 +298,7 @@ const getThemeFonts = (req, res)=>{
 		}
 	};
 
-	// Header stuff for allowing the css to access fonts
+	// Header stuff for allowing sites to make cross origin requests for the fonts, required for the css to work
 	res.setHeader('Access-Control-Allow-Origin',  '*');
 	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept');
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
@@ -348,7 +349,7 @@ const getThemeSnippet = (req, res, next)=>{
 				snippetOutput = snippet['gen']();
 			}
 			catch {
-				return res.status(500).send("Malformed snippet (function");
+				return res.status(500).send("Malformed snippet (function)");
 			}
 		}
 		else if(typeof snippet['gen'] == 'string' || snippet['gen'] instanceof String) {
@@ -362,10 +363,10 @@ const getThemeSnippet = (req, res, next)=>{
 		if (error.code !== 'MODULE_NOT_FOUND') {
 			throw error;
 		}
-		return res.status(404).send("No such file: " + req.params.path );
+		return res.status(404).send("No such module: " + req.params.path );
 	}
 
-	// Header stuff for allowing the css to access fonts
+	// Header stuff for allowing sites to make cross origin requests for the snippets
 	res.setHeader('Access-Control-Allow-Origin',  '*');
 	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept');
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
